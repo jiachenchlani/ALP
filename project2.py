ee59272540e140d2234f01277798ef0d1dc6d2d4
jiachenchlani <47284628+jiachenchlani@users.noreply.github.com>
2025-07-14 09:28:19 -0500

    Update project2.py

--- project2.py
+++ project2.py
@@ -5,41 +5,50 @@ import matplotlib.pyplot as plt
 # DEFINE CONSTANTS
 a = 1 # infection rate
 b = 1/14 # recovery rate
+d_r = 0.01 
 
 # FUNCTION TO RETURN DERIVATIVES AT T
 def f(y,t):
-	S, I, _ = y # get previous values of S, I, R and store them in array y
+	S, I, R, D = y # get previous values of S, I, R and store them in array y
 	d0 = -a*S*I # derivative of S(t)
-	d1 = a*S*I - b*I # derivative of I(t)
+	d1 = a*S*I - b*I - d_r*I # derivative of I(t)
 	d2 = b*I # derivative of R(t)
+	d3 = d_r*I
 
-	return [d0, d1, d2]
+	return [d0, d1, d2, d3]
 
 # INITIAL VALUES OF EACH FUNCTION 
 S_0 = 1
 I_0 = 3.125/(10**6)
 R_0 = 0
-y_0 = [S_0,I_0,R_0]
+D_0 = 0
+y_0 = [S_0,I_0,R_0,D_0]
 
-t = np.linspace(start=1,stop=100,num=100)
+t = np.linspace(start=1,stop=100,num=101)
 y = odeint(f,y_0,t) 
 
 S = y[:,0]
 I = y[:,1]
 R = y[:,2]
+D = y[:,3]
 
 plt.figure()
 plt.plot(t,S,"r",label="S(t)")
 plt.plot(t,I,'b',label="I(t)")
 plt.plot(t,R,'g',label="R(t)")
+plt.plot(t, D, "k", label="D(t)")
+peak_o = t[np.argmax(I)]
+plt.axvline(peak_o, color='gray', linestyle='--', label="Peak Infections")
+plt.title("SIRD Model w odeint")
+plt.xlabel("Time (days)")
+plt.ylabel("Fraction of Population")
 plt.legend()
 plt.show()
-
-import matplotlib.pyplot as plt
 
 s_cache = {}
 i_cache = {}
 r_cache = {}
+d_cache = {}
 
 def s(t):
 	if t == 0:
@@ -63,7 +72,7 @@ def i(t):
 		value = i_cache[t]
 		return value
 
-	result = i(t-1)+(s(t-1)*i(t-1))-((1/14)*i(t-1))
+	result = i(t-1) + (s(t-1)*i(t-1)) - ((1/14)*i(t-1)) - (d_r * i(t-1))
 
 	i_cache[t] = result
 
@@ -83,23 +92,59 @@ def r(t):
 
 	return result
 
+def d(t):
+    if t == 0:
+        return 0
+    if t in d_cache:
+        return d_cache[t]
+
+    result = d(t-1) + d_r * i(t-1)
+    d_cache[t] = result
+    return result
+
 plt.figure()
 s_list = []
 i_list = []
 r_list = []
-x = [t for t in range(0,101)]
-for t in range(0,101):
-	s_list.append(s(t))
-	i_list.append(i(t))
-	r_list.append(r(t))
+d_list = []
+time_list = list(range(0, 101))
+for day in range(0,101):
+	s_list.append(s(day))
+	i_list.append(i(day))
+	r_list.append(r(day))
+	d_list.append(d(day))
 
-plt.plot(x,s_list,"r",label="s(t)")
-plt.plot(x,i_list,'b',label="i(t)")
-plt.plot(x,r_list,'g',label="r(t)")
+plt.plot(time_list,s_list,"r",label="s(t)")
+plt.plot(time_list,i_list,'b',label="i(t)")
+plt.plot(time_list,r_list,'g',label="r(t)")
+plt.plot(time_list, d_list, "k", label="d(t)")
+peak_r = time_list[np.argmax(i_list)]
+plt.axvline(peak_r, color='gray', linestyle='--', label="Peak Infection")
 plt.ylim((0, 1)) 
+plt.title("SIRD Model w/ Recursion")
+plt.xlabel("Time (days)")
+plt.ylabel("Fraction of Population")
 plt.legend()
 plt.show()
 
-print(str(s_list), file=open("output.txt", "a"))
-print(str(i_list), file=open("output.txt", "a"))
-print(str(r_list), file=open("output.txt", "a"))
+plt.figure()
+
+# odeint
+plt.plot(t, S, "r--", label="S (odeint)")
+plt.plot(t, I, "b--", label="I (odeint)")
+plt.plot(t, R, "g--", label="R (odeint)")
+plt.plot(t, D, "k--", label="D (odeint)")  
+
+# recurszion
+plt.plot(time_list, s_list, "r", label="S (recursion)")
+plt.plot(time_list, i_list, "b", label="I (recursion)")
+plt.plot(time_list, r_list, "g", label="R (recursion)")
+plt.plot(time_list, d_list, "k", label="D (recursion)")
+
+plt.title("SIRD Model: odeint vs Recursion")
+plt.xlabel("Time (days)")
+plt.ylabel("Fraction of Population")
+plt.legend()
+plt.ylim(0, 1)
+plt.grid(True)
+plt.show()
